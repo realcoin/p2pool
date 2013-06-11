b68c947b84eccdac1cdf225494f75f29ea94b465
dotcoin-dev <dotcoin@live.com>
2013-06-11 14:15:03 +0800

    modify

--- p2ppool.bat
+++ p2ppool.bat
@@ -1,1 +1,1 @@
-python run_p2pool.py --net realcoin -a RYZfywANBkdM2kTDVpAx4rKP3oZdiftGNj -f 1 -a RYZfywANBkdM2kTDVpAx4rKP3oZdiftGNj --max-conns 256 --give-author 0
+screen -d -m -S RealCoinP2Pool /root/realcoin/p2pool/run_p2pool.py --net realcoin -a RYZfywANBkdM2kTDVpAx4rKP3oZdiftGNj -f 1 --max-conns 256 --give-author 0
